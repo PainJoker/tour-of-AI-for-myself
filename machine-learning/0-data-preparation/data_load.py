3393800bdc1db9f8e70786b008cc5dc1f37afc1f
PainJoker <painjoker42@gmail.com>
2023-12-15 22:36:24 +0800

    ✨ feat: Add scaling algo
    Add the normalization and standardization algorithm for data scaling.

--- machine-learning/0-data-preparation/data_load.py
+++ machine-learning/0-data-preparation/data_load.py
@@ -1,5 +1,6 @@
 # Example of loading CSV dataset
 import csv
+from math import sqrt
 
 # Load a CSV file
 def load_csv(filename):
@@ -27,6 +28,46 @@ def str_column_to_int(dataset, col):
         row[col] = lookup[row[col]] # Replace the string
     return lookup
 
+# Find the max and min of each attr in dataset
+def dataset_maxmin(dataset):
+    maxmin = list()
+    for i in range(len(dataset[0])):
+        col_value = [row[i] for row in dataset]
+        col_max = max(col_value)
+        col_min = min(col_value)
+        maxmin.append([col_max, col_min])
+    return maxmin
+    
+# Rescale dataset columns to the range [0-1]
+def normalize_dataset(dataset, maxmin):
+    for row in dataset:
+        for i in range(len(dataset[0])):
+            row[i] = (row[i]-maxmin[i][1]) / (maxmin[i][0]-maxmin[i][1])
+    
+# Calculate column means
+def col_means(dataset):
+    means = [0 for i in range(len(dataset[0]))]
+    for i in range(len(dataset[0])):
+        col_values = [row[i] for row in dataset]
+        means[i] = sum(col_values) / float(len(col_values))
+    return means
+
+# Calculate column standard deviation
+def col_stdevs(dataset, means):
+    stdevs = [0 for i in range(len(dataset[0]))]
+    for i in range(len(dataset[0])):
+        variance = [pow(row[i]-means[i]) for row in dataset]
+        stdevs[i] = sum(variance)
+    stdevs = [sqrt(item)/float(len(dataset[0])-1) for item in stdevs]    
+    return stdevs
+
+# Standardize the dataset
+# Assume the sampling data is normally distributed
+def standardize_dataset(dataset, means, stdevs):
+    for row in dataset:
+         for i in range(len(dataset[0])):
+             row[i] = (row[i] - means[i]) / stdevs[i]
+
 # Load dataset
 pima_filename = "pima-indians-diabetes.csv"
 pima_dataset = load_csv(pima_filename)
@@ -37,10 +78,16 @@ print(f"The first element is {pima_dataset[0]}.")
 for i in range(len(pima_dataset[0])):
     str_column_to_float(pima_dataset, i)
 print(f"After operation: {pima_dataset[0]}.")
+# Obtain the range of attr
+pima_maxmin = dataset_maxmin(pima_dataset)
+print(f"The max and min of each attr are {pima_maxmin}.")
+# Normalize the columns
+normalize_dataset(pima_dataset, pima_maxmin)
+print(f"After normalization, the first list is {pima_dataset[0]}.")
 
 iris_filename = "iris.csv"
 iris_dataset = load_csv(iris_filename)
-print(f"Loaded data file {iris_filename} with {len(iris_dataset)} rows \
+print(f"\nLoaded data file {iris_filename} with {len(iris_dataset)} rows \
 and {len(iris_dataset[0])} columns.")
 print(f"The first element is {iris_dataset[0]}.")
 # Convert string column to float
